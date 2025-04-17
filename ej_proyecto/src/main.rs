58a066d9f4c278018dfece535a00fd9cb5cb857c
BRivsC <bastian.rivas@sansano.usm.cl>
2025-04-17 01:58:22 -0400

    Agregado código para encender LED con botón

--- ej_proyecto/src/main.rs
+++ ej_proyecto/src/main.rs
@@ -10,9 +10,37 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 use cortex_m::asm;
 use cortex_m_rt::entry;
 
-#[entry]
+use cortex_m_rt as rt;
+use stm32f4xx_hal as hal;
+use hal::prelude::*; // needed for the GpioExt trait (-> .split)
+
+//#[entry]
+#[rt::entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    //asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    // El siguiente código fue tomado de https://github.com/krenzlin/rust-stm32f446-blinky/blob/master/src/main.rs
+    if let Some(peripherals) = hal::stm32::Peripherals::take() {
+        let gpioa = peripherals.GPIOA.split(); // + sets RCC->AHB1ENR GPIOA bit
+
+        // .into_push_pull_output performs three steps
+        // 1) set PUPDR: 00 -> no pull-up, no pull-down
+        // 2) set OTYPER: 0 -> output push-pull
+        // 3) set MODER: 01 -> general purpose output mode
+        let mut led = gpioa.pa5.into_push_pull_output();
+
+        let gpioc = peripherals.GPIOC.split();
+        let button = gpioc.pc13; // pins are input by default
+
+        loop {
+            // .is_high reads IDR
+            if button.is_high().unwrap() {
+                // .set_low uses BSRR
+                led.set_low().unwrap();
+            } else {
+                led.set_high().unwrap();
+            }
+        }
+    }
 
     loop {
         // your code goes here
