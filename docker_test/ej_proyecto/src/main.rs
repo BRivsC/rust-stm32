55f982e5912136b0d959f5729d0721a7ea086467
BRivsC <bastian.rivas@sansano.usm.cl>
2025-05-08 00:58:58 -0400

    Agregado un ejemplo para probar Docker

---
+++ docker_test/ej_proyecto/src/main.rs
@@ -0,0 +1,60 @@
+#![no_std]
+#![no_main]
+/// Ejemplo 1: Encender LED al pulsar el botón en la placa
+/// Bastián Rivas
+/// Este ejemplo fue hecho pensando en una placa STM32 NUCLEO-F411RE
+/// 
+/// Compilar: cd ej_proyecto
+///           cargo build --target thumbv7em-none-eabihf
+/// Flashear: cargo embed
+
+
+
+// pick a panicking behavior
+use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+// use panic_abort as _; // requires nightly
+// use panic_itm as _; // logs messages over ITM; requires ITM support
+// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+
+//use cortex_m::asm;
+//use cortex_m_rt::entry;
+
+use cortex_m_rt as rt;
+use stm32f4xx_hal as hal;
+use hal::prelude::*; // needed for the GpioExt trait (-> .split)
+use rtt_target::{rprintln, rtt_init_print}; //  Permite comunicarse usando RTT
+
+//#[entry]
+#[rt::entry]
+fn main() -> ! {
+    // El siguiente código fue tomado de https://github.com/krenzlin/rust-stm32f446-blinky/blob/master/src/main.rs
+    rtt_init_print!();
+    if let Some(peripherals) = hal::stm32::Peripherals::take() {
+        let gpioa = peripherals.GPIOA.split(); // + sets RCC->AHB1ENR GPIOA bit
+
+        // .into_push_pull_output performs three steps
+        // 1) set PUPDR: 00 -> no pull-up, no pull-down
+        // 2) set OTYPER: 0 -> output push-pull
+        // 3) set MODER: 01 -> general purpose output mode
+        let mut led = gpioa.pa5.into_push_pull_output();
+
+        let gpioc = peripherals.GPIOC.split();
+        let button = gpioc.pc13; // pins are input by default
+
+        loop {
+            // .is_high reads IDR
+            if button.is_high() {
+                // .set_low uses BSRR
+                led.set_low();
+                rprintln!("Botón libre");
+            } else {
+                led.set_high();
+                rprintln!("Botón pulsado!");
+            }
+        }
+    }
+
+    loop {
+        // your code goes here
+    }
+}
